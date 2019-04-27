8082a43d67c7ac8a67861cb2af64ad2d3f307437
권재원 <springkjw@gmail.com>
2019-04-27 14:20:34 +0900

    카페 상세 화면

---
+++ CafeListModel.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+final class CafeListModel: CafeModelInput {
+    weak var output: Cafe
+}
---
+++ CafeListModelOutput.swift
@@ -0,0 +1,10 @@
+//
+//  CafeModelOutput.swift
+//  YourCafe
+//
+//  Created by 권재원 on 13/04/2019.
+//  Copyright © 2019 이동건. All rights reserved.
+//
+
+import Foundation
+
